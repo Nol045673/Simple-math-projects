347705b1c73aa0ea5be9696045045ce086de8c67
Nol03 <59832719+Nol045673@users.noreply.github.com>
2021-05-21 02:17:44 +0300

    Add files via upload

---
+++ MainikFor1Nomber.java
@@ -0,0 +1,30 @@
+package komer.nol.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView one = (TextView)findViewById(R.id.morecat);
+        ImageButton kitty = (ImageButton)findViewById(R.id.imageButton4);
+
+        kitty.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                one.setText("Nyaaaaa");
+            }
+        });
+
+    }
+}
